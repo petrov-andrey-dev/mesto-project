c8455cf2934503e0ca10fade3889bb8986c32168
petrov-andrey-dev <petrov.a.v-14@yandex.ru>
2023-04-18 18:10:13 +0300

    develop/fix fixed added post function, rename functions, other fixes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const editProfileBtn = document.querySelector('.profile__edit-info');
-const popupCloseBtn = document.querySelectorAll('.popup__close');
-const popupForm = document.querySelectorAll('.popup__form');
+const popupCloseBtns = document.querySelectorAll('.popup__close');
+const popupForms = document.querySelectorAll('.popup__form');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
 const nameInput = document.querySelector('#name');
@@ -9,99 +9,99 @@ const popupEdit = document.querySelector('.popup_type_edit');
 const popupAdd = document.querySelector('.popup_type_add');
 const popupImage = document.querySelector('.popup_type_image');
 const addPostBtn = document.querySelector('.profile__add-post');
+const postGrid = document.querySelector('.posts__grid');
+const bigImage = document.querySelector('.popup__big-image');
+const figcaption = document.querySelector('.popup__figcaption');
+const captionInput = document.querySelector('#caption');
+const linkInput = document.querySelector('#link');
 
 const initialCards = [
     {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
     },
     {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
     },
     {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
     },
     {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
     },
     {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
     },
     {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
-  ]; 
+];
 
-initialCards.forEach(card => {addPost(card.link, card.name);})
+initialCards.forEach(card => postGrid.append(createPost(card.link, card.name)));
 
-function popupOpen(element) {
+function openPopup(element) {
     element.classList.add('popup_opened');
 }
 
-function popupClose(element) {
+function closePopup(element) {
     element.classList.remove('popup_opened');
 }
 
-function addPost(link, name) {
-    const postGrid = document.querySelector('.posts__grid');
+function createPost(link, name) {
     const post = document.querySelector('#post').content;
+    const postElement = post.querySelector('.post').cloneNode(true);
+    const postImg = postElement.querySelector('.post__img');
 
-    if (link && name) {
-        const postElement = post.querySelector('.post').cloneNode(true);
-        postElement.querySelector('.post__img').src = link;
-        postElement.querySelector('.post__title').textContent = name;
-        postElement.querySelector('.post__img').addEventListener('click', (evt) => {
-            popupOpen(popupImage);
-            openImage(evt);
-        })
-        postElement.querySelector('.post__like').addEventListener('click', (evt) => { evt.target.classList.toggle('post__like_liked') });
-        postElement.querySelector('.post__trash').addEventListener('click', (evt) => { evt.target.closest('.post').remove() });
-        postGrid.prepend(postElement);
-    } else {
-        console.log('Поля "Название" и "Ссылка" не должны быть пустыми');
-    }
+    postImg.src = link;
+    postImg.alt = name;
+    postElement.querySelector('.post__title').textContent = name;
+    postImg.addEventListener('click', (evt) => {
+        openPopup(popupImage);
+        openImage(evt);
+    })
+    postElement.querySelector('.post__like').addEventListener('click', (evt) => { evt.target.classList.toggle('post__like_liked') });
+    postElement.querySelector('.post__trash').addEventListener('click', (evt) => { evt.target.closest('.post').remove() });
+    return postElement;
 }
 
 function openImage(evt) {
-    const bigImage = document.querySelector('.popup__big-image');
-    const figcaption = document.querySelector('.popup__figcaption');
-
+    const parentPostTitle = evt.target.parentNode.querySelector('.post__title');
     bigImage.src = evt.target.src;
-    figcaption.textContent = evt.target.parentNode.querySelector('.post__title').textContent;
+    bigImage.alt = parentPostTitle.textContent;
+    figcaption.textContent = parentPostTitle.textContent;
 }
 
 function editProfile() {
-    popupOpen(popupEdit);
+    openPopup(popupEdit);
     nameInput.value = profileName.textContent;
     descriptionInput.value = profileDescription.textContent;
 }
 
-function popupSubmit(evt) {
+function submitPopup(evt) {
     evt.preventDefault();
     if (evt.target.closest('.popup_type_edit')) {
         profileName.textContent = nameInput.value;
         profileDescription.textContent = descriptionInput.value;
     } else if (evt.target.closest('.popup_type_add')) {
-        const captionInput = document.querySelector('#caption');
-        const linkInput = document.querySelector('#link');
-        addPost(linkInput.value, captionInput.value);
+        postGrid.prepend(createPost(linkInput.value, captionInput.value));
     }
-    popupClose(evt.target.closest('.popup'));
+    closePopup(evt.target.closest('.popup'));
+    evt.target.reset();
 }
 
 editProfileBtn.addEventListener('click', editProfile);
 
-addPostBtn.addEventListener('click', () => {popupOpen(popupAdd)});
+addPostBtn.addEventListener('click', () => { openPopup(popupAdd) });
 
-popupCloseBtn.forEach(element => {
+popupCloseBtns.forEach(element => {
     element.addEventListener('click', () => {
-        popupClose(element.closest('.popup'));
+        closePopup(element.closest('.popup'));
     })
 });
 
-popupForm.forEach(form => {form.addEventListener('submit', popupSubmit)});
+popupForms.forEach(form => { form.addEventListener('submit', submitPopup) });
