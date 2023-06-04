f9c0dc470c99b83ed9283e19f6d5db3e05d8e3b7
Anna Kyzina <am@MacBook-Air-Anna.local>
2023-06-04 12:53:25 +0300

    refactor:переписать открытиея popup на ООП

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,61 +1,106 @@
-import {
-    popupEdit,
-    nameInput,
-    descriptionInput,
-    profileName,
-    profileDescription,
-    popupImage
-} from "../index.js";
+// import {
+//     popupEdit,
+//     nameInput,
+//     descriptionInput,
+//     profileName,
+//     profileDescription,
+//     popupImage
+// } from "../index.js";
 // import { openImage } from "./card.js";
+//-------------------------->      ООП Popup       <------------------------------------
 
-// закрытие попапа по Escape
-function closeByEsc(evt) {
-    if (evt.key === 'Escape') {
-        const openPopup = document.querySelector('.popup_opened');
-        closePopup(openPopup);
+export default class Popup {
+    // Принимает в конструктор единственный параметр — селектор попапа
+    constructor(popupSelector) {
+        this._popup = document.querySelector(popupSelector);
     }
-};
+    open() {
+        this._popup.classList.add("popup_opened");
+        document.addEventListener("keydown", this._handleEscClose);
+    }
+    close() {
+        this._popup.classList.remove("popup_opened");
+        document.removeEventListener("keydown", this._handleEscClose);
+    }
+    //   закрытие попапа клавишей Esc
+    _handleEscClose(evt) {
+        if (evt.key === "Escape") {
+            this.close();
+        }
+    }
 
-//отрисовка кнопки сабмита на форме
-function renderSubmitBtn(isLoading, button, buttonText = 'Сохранить', loadingText = 'Сохранение...') {
-    if (isLoading) {
-        button.textContent = loadingText
-    } else {
-        button.textContent = buttonText
+    setEventListeners() {
+        this._popup.addEventListener("mousedown", this._handlePopupClose);
     }
+    _handlePopupClose = (evt) => {
+        if (
+            evt.target.classList.contains("popup_opened") ||
+            evt.target.classList.contains("popup__close")
+        ) {
+            this.close();
+        }
+    };
+    // function handlePopupClose(popups) {
+    //     popups.forEach(item => {
+    //         item.addEventListener('mousedown', evt => {
+    //             if (evt.target.classList.contains('popup')) {
+    //                 closePopup(evt.target);
+    //             } else if (evt.target.classList.contains('popup__close')) {
+    //                 closePopup(evt.target.closest('.popup'));
+    //             }
+    //         });
+    //     });
+    // };
 }
 
+// закрытие попапа по Escape
+// function closeByEsc(evt) {
+//     if (evt.key === 'Escape') {
+//         const openPopup = document.querySelector('.popup_opened');
+//         closePopup(openPopup);
+//     }
+// };
+
+//отрисовка кнопки сабмита на форме
+// function renderSubmitBtn(isLoading, button, buttonText = 'Сохранить', loadingText = 'Сохранение...') {
+//     if (isLoading) {
+//         button.textContent = loadingText
+//     } else {
+//         button.textContent = buttonText
+//     }
+// }
+
 //открытие попапа
-function openPopup(element) {
-    element.classList.add('popup_opened');
-    document.addEventListener('keydown', closeByEsc);
-};
+// function openPopup(element) {
+//     element.classList.add('popup_opened');
+//     document.addEventListener('keydown', closeByEsc);
+// };
 
 //закрытие попапа
-function closePopup(element) {
-    element.classList.remove('popup_opened');
-    document.removeEventListener('keydown', closeByEsc);
-};
+// function closePopup(element) {
+//     element.classList.remove('popup_opened');
+//     document.removeEventListener('keydown', closeByEsc);
+// };
 
-//обработчик кнопки изменения профиля
-function editProfile() {
-    openPopup(popupEdit);
-    nameInput.value = profileName.textContent;
-    descriptionInput.value = profileDescription.textContent;
-};
+// обработчик кнопки изменения профиля
+// function editProfile() {
+//     openPopup(popupEdit);
+//     nameInput.value = profileName.textContent;
+//     descriptionInput.value = profileDescription.textContent;
+// };
 
 //закрытие попапов по оверлею или крестику
-function handlePopupClose(popups) {
-    popups.forEach(item => {
-        item.addEventListener('mousedown', evt => {
-            if (evt.target.classList.contains('popup')) {
-                closePopup(evt.target);
-            } else if (evt.target.classList.contains('popup__close')) {
-                closePopup(evt.target.closest('.popup'));
-            }
-        });
-    });
-};
+// function handlePopupClose(popups) {
+//     popups.forEach(item => {
+//         item.addEventListener('mousedown', evt => {
+//             if (evt.target.classList.contains('popup')) {
+//                 closePopup(evt.target);
+//             } else if (evt.target.classList.contains('popup__close')) {
+//                 closePopup(evt.target.closest('.popup'));
+//             }
+//         });
+//     });
+// };
 
 // обработчик открытия картинки
 // function handleImageOpen(evt) {
@@ -64,24 +109,26 @@ function handlePopupClose(popups) {
 // };
 
 // универсальный обработчик сабмита
-function handleSubmit(request, evt, loadingText = "Сохранение...") {
-    evt.preventDefault();
-    const submitButton = evt.submitter;
-    const initialText = submitButton.textContent;
-    renderSubmitBtn(true, submitButton, initialText, loadingText);
-    request()
-        .then(() => {
-            closePopup(evt.target.closest('.popup'));
-            evt.target.reset();
-        })
-        .catch((err) => {
-            console.error(`Ошибка: ${err}`);
-        })
-        .finally(() => {
-            renderSubmitBtn(false, submitButton, initialText);
-        });
-};
+// function handleSubmit(request, evt, loadingText = "Сохранение...") {
+//     evt.preventDefault();
+//     const submitButton = evt.submitter;
+//     const initialText = submitButton.textContent;
+//     renderSubmitBtn(true, submitButton, initialText, loadingText);
+//     request()
+//         .then(() => {
+//             closePopup(evt.target.closest('.popup'));
+//             evt.target.reset();
+//         })
+//         .catch((err) => {
+//             console.error(`Ошибка: ${err}`);
+//         })
+//         .finally(() => {
+//             renderSubmitBtn(false, submitButton, initialText);
+//         });
+// };
 
-export { openPopup, closePopup, renderSubmitBtn, editProfile, handlePopupClose, 
-    // handleImageOpen, 
-    handleSubmit };
+//export {
+//openPopup, closePopup, renderSubmitBtn, editProfile, handlePopupClose,
+///handleImageOpen,
+//     handleSubmit
+//};
