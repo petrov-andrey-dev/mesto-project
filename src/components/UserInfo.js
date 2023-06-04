7b02a654649773255656f5e6426dd0f7e3c2c21d
Anna Kyzina <am@MacBook-Air-Anna.local>
2023-06-04 13:11:27 +0300

    refactor:добавить модуль UserInfo для для дальнейшей доработки логики

---
+++ src/components/UserInfo.js
@@ -0,0 +1,22 @@
+export class UserInfo {
+    constructor(nameSelector, descriptionSelector, avatarSelector) {
+        this._name = document.querySelector(nameSelector);
+        this._description = document.querySelector(descriptionSelector);
+        this._avatar = document.querySelector(avatarSelector);
+    }
+//получает данные пользователя при открытии формы
+    getUserInfo() {
+        this._info = {
+            name: this._name.textContent,
+            job: this._description.textContent,
+            avatar: this._avatar.src
+        }
+        return this._info;
+    }
+//принимает новые данные пользователя и добавляет их на страницу
+    setUserInfo(data) {
+        this._name.textContent = data.name;
+        this._description.textContent = data.job;
+        this._avatar.src = data.avatar;
+    }
+}
